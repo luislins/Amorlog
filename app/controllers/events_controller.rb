a7163e745559d4cc5fcf69a553d85b19f4fd6161
Luis Guilherme <luis@donorbox.org>
2024-11-09 15:04:35 -0300

    create events e couple

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,31 @@
+class EventsController < ApplicationController
+    before_action :set_couple
+  
+    def index
+      @events = @couple.events
+    end
+  
+    def new
+      @event = @couple.events.new
+    end
+  
+    def create
+      @event = @couple.events.new(event_params)
+      if @event.save
+        redirect_to couple_events_path(@couple), notice: "Evento criado com sucesso!"
+      else
+        render :new
+      end
+    end
+  
+    private
+  
+    def set_couple
+      @couple = Couple.find_by(slug: params[:couple_slug])
+    end
+  
+    def event_params
+      params.require(:event).permit(:title, :description, :date, :event_type)
+    end
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "couples#new" # Define a página inicial como o formulário de criação de casal
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :couples, param: :slug do
+    resources :events
+  end
 end
