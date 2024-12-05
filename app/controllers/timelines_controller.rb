66e92ce165909419134d31d9a10eabe6f431ef67
Luis Guilherme <luis@donorbox.org>
2024-12-05 00:09:00 -0300

    view de timeline

---
+++ app/controllers/timelines_controller.rb
@@ -0,0 +1,42 @@
+class TimelinesController < ApplicationController
+  before_action :set_couple
+  def index
+    couple = Couple.find_by!(slug: params[:couple_slug])
+
+    events = couple.events.map do |event|
+      {
+        type: :event,
+        title: event.title,
+        description: event.description,
+        date: event.date,
+        event_type: event.event_type
+      }
+    end
+
+    goals = couple.goals.map do |goal|
+      {
+        type: :goal,
+        title: goal.title,
+        description: goal.description,
+        date: goal.due_date,
+        achieved: goal.achieved,
+        kind: goal.kind == 1 ? 'numeric' : 'boolean',
+        numeric_value: goal.numeric_value,
+        current_value: goal.current_value
+      }
+    end
+
+    @timeline_items = (events + goals).sort_by { |item| item[:date] }
+  end
+
+  private
+
+  def set_couple
+    @couple = Couple.find_by(slug: params[:couple_slug])
+
+    # Verifica se o casal pertence ao usuário autenticado
+    return unless @couple.nil? || @couple.user != current_user
+
+      redirect_to root_path, alert: 'Acesso não autorizado.'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'timelines/index'
   get 'home/index'
   root to: 'home#index'
 
   devise_for :users, controllers: { registrations: 'users/registrations' }
-
+  
   resources :couples, param: :slug do
+    get 'timeline', to: 'timelines#index', as: 'timeline'
     resources :events
     resources :goals do
       member do
