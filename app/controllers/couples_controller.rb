ef096db8d8def5478188b75e21679f4800367330
Luis Guilherme <luis@donorbox.org>
2024-12-07 16:25:45 -0300

    inicio tela edição nomes

--- app/controllers/couples_controller.rb
+++ app/controllers/couples_controller.rb
@@ -6,9 +6,26 @@ class CouplesController < ApplicationController
       @couple = Couple.find_by(slug: params[:slug])
     end
 
+    def update
+      if @couple.update(couple_params)
+        redirect_to edit_couple_path(@couple), notice: 'Dados do casal atualizados com sucesso.'
+      else
+        flash.now[:alert] = 'Erro ao atualizar os dados do casal.'
+        render :edit
+      end
+    end
+
     private
 
+    def set_couple
+      @couple = Couple.find_by(slug: params[:slug])
+  
+      return unless @couple.nil? || @couple.user != current_user
+  
+      redirect_to root_path, alert: 'Acesso não autorizado.'
+    end
+
     def couple_params
-      params.require(:couple).permit(:name)
+      params.require(:couple).permit(:name, :partner_1, :partner_2)
     end
 end
