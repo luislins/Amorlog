58a05494316bf2dc76bc88aee5d3508c9242a74b
Luis Guilherme <luis@donorbox.org>
2024-12-09 10:26:43 -0300

    aajustando contador

--- app/javascript/controllers/countdown_controller.js
+++ app/javascript/controllers/countdown_controller.js
@@ -1,8 +1,12 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static targets = ["days", "hours", "minutes", "seconds"];
-  static values = { targetDate: String }
+  static values = { targetDate: String };
+
+  spinner_class = "absolute border-4 border-t-white border-gray-400 rounded-full w-10 h-10 animate-spin";
+  number_class = "absolute text-4xl font-bold text-loveRed opacity-1 transition-opacity duration-200";
+  first_time = true;
 
   connect() {
     this.targetTime = new Date(this.targetDateValue).getTime();
@@ -16,7 +20,6 @@ export default class extends Controller {
     clearInterval(this.interval);
   }
 
-
   updateCountdown() {
     const now = new Date().getTime();
     const timeRemaining = Math.abs(this.targetTime - now);
@@ -27,9 +30,28 @@ export default class extends Controller {
     const minutes = Math.floor((totalSeconds % (60 * 60)) / 60);
     const seconds = Math.floor(totalSeconds % 60);
 
-    if (this.hasDaysTarget) this.daysTarget.textContent = days;
-    if (this.hasHoursTarget) this.hoursTarget.textContent = hours;
-    if (this.hasMinutesTarget) this.minutesTarget.textContent = minutes;
-    if (this.hasSecondsTarget) this.secondsTarget.textContent = seconds;
+    this.updateTarget(this.daysTarget, days);
+    this.updateTarget(this.hoursTarget, hours);
+    this.updateTarget(this.minutesTarget, minutes);
+    this.updateTarget(this.secondsTarget, seconds);
+
+    if (this.first_time) {
+      this.first_time = false;
+    }
+  }
+
+  updateTarget(target, value) {
+    if (!target) return;
+
+    if (this.first_time) {
+      target.classList.add("opacity-0");
+    }
+
+    setTimeout(() => {
+      target.textContent = value;
+
+      target.classList.remove("opacity-0");
+      target.className = this.number_class;
+    }, this.first_time ? 200 : 0);
   }
 }
