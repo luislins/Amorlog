b5ca51915abe49fdc7e7e2c8a5ee4c898bac1790
Luis Guilherme <luisguilherme.siqueira32@gmail.com>
2025-01-13 21:33:07 -0300

    ajustes home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,10 @@
 class ApplicationController < ActionController::Base
  before_action :authenticate_user!
  before_action :set_couple, if: :user_signed_in?
+ 
  private
 
   def set_couple
     @couple = current_user.couple
-
-    if @couple.nil?
-      redirect_to root_path, alert: 'Você ainda não possui um casal associado.'
-    end
   end
 end
